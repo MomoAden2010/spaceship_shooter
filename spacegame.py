054f2d5c85b1b4015e59cda6c5cc66ebcfcfd495
MomoAden2010 <momoadenmosun1@gmail.com>
2025-06-23 18:59:50 +0100

    Add files via upload

---
+++ spacegame.py
@@ -0,0 +1,31 @@
+import random
+import pgzrun
+
+WIDTH = 700
+HEIGHT = 650
+
+spaceship = Actor("spaceship.png")
+spaceship.pos = 100,350
+bug = Actor("bug.png")
+
+enemies = []
+
+def draw():
+    screen.clear()
+    screen.fill((242, 150, 219))
+    bug.draw()
+    spaceship.draw()
+
+def update():
+    if keyboard.left:
+        spaceship.x = spaceship.x-2
+    if keyboard.right:
+        spaceship.x = spaceship.x+2
+
+#def create_bugs():
+    for i in range(8):
+        for j in range(4):
+            enemies.append(Actor("bug"))
+
+
+pgzrun.go()
